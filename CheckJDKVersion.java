f32af91dd156d8fcbdcaefbc3e9bc48d447d7182
Starman7312 <henrygillett@duck.com>
2025-09-29 17:49:43 +0100

    feat: user friendly check added

--- CheckJDKVersion.java
+++ CheckJDKVersion.java
@@ -1,19 +1,19 @@
 public class CheckJDKVersion {
     public static void main(String[] args) {
         System.out.println("=== Java Runtime Information ===");
-        System.out.println("Java Version: " + System.getProperty("java.version"));
-        System.out.println("Java Vendor: " + System.getProperty("java.vendor"));
-        System.out.println("Java Home: " + System.getProperty("java.home"));
-        System.out.println("Java Runtime Version: " + System.getProperty("java.runtime.version"));
-        System.out.println("Java VM Name: " + System.getProperty("java.vm.name"));
-        System.out.println("Java VM Version: " + System.getProperty("java.vm.version"));
-        System.out.println("Java VM Vendor: " + System.getProperty("java.vm.vendor"));
-        System.out.println("Operating System: " + System.getProperty("os.name"));
-        System.out.println("OS Version: " + System.getProperty("os.version"));
-        System.out.println("OS Architecture: " + System.getProperty("os.arch"));
-        System.out.println("User Name: " + System.getProperty("user.name"));
-        System.out.println("User Home: " + System.getProperty("user.home"));
-        System.out.println("Current Working Directory: " + System.getProperty("user.dir"));
-        System.out.println("Java Class Path: " + System.getProperty("java.class.path"));
+
+        String version = System.getProperty("java.version");
+        System.out.println("Java Version: " + version);
+
+        String vendor = System.getProperty("java.vendor");
+        System.out.println("Java Vendor: " + vendor);
+        
+        if (!(vendor.equals("Eclipse Adoptium")) || !(version.substring(0, 2).equals("17"))){
+            System.out.println("\nInvalid Java Version In Use, Vendor: " + vendor + ", Version: " + version);
+            System.out.println("Please use Vendor: Eclipse Adoptium, Version: 17.0.16");
+        }
+        else{
+            System.out.println("\nJava runtime version is correct");
+        }
     }
 }
