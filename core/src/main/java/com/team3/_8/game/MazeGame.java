fadeb9ee4fe7627fbb24518eed925760ae1989a8
Lenny Schwarz <lennyschwarz06@gmail.com>
2025-10-13 10:20:46 +0100

    feat: Made Bob move on the X axis within bounds

--- core/src/main/java/com/team3/_8/game/MazeGame.java
+++ core/src/main/java/com/team3/_8/game/MazeGame.java
@@ -2,6 +2,8 @@ package com.team3._8.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -12,7 +14,9 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class MazeGame extends ApplicationAdapter {
     private SpriteBatch batch;
 
-    private Texture alonso;
+//    private Texture alonso;
+
+    private Texture bob;
 
     private Sprite sprite;
 
@@ -20,14 +24,18 @@ public class MazeGame extends ApplicationAdapter {
     public void create() {
         batch = new SpriteBatch();
 
-        alonso = new Texture("alonso.jpg");
+//        alonso = new Texture("alonso.jpg");
+        bob = new Texture("bob.jpg");
 
-        sprite = new Sprite(alonso);
-        sprite.setBounds(256, 320, 512, 384);
+        sprite = new Sprite(bob);
+        sprite.setBounds(0, 0, 320, 200);
     }
 
     @Override
     public void render() {
+
+        input();
+
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
 
         batch.begin();
@@ -41,13 +49,28 @@ public class MazeGame extends ApplicationAdapter {
     public void dispose() {
         batch.dispose();
 
-        alonso.dispose();
+        bob.dispose();
     }
 
     @Override
     public void resize(int width, int height) {
         System.out.println(Math.abs(width - 1024)/2);
         batch.getProjectionMatrix().setToOrtho2D(0, 0, width, height);
-//        sprite.setBounds(Math.abs(width - 1024)/2, Math.abs(height - 384)/2, width, height);
+    }
+
+    private void input(){
+        float speed = 40f;
+        float delta = Gdx.graphics.getDeltaTime();
+
+
+        if ((Gdx.input.isKeyPressed(Input.Keys.RIGHT)) && (sprite.getX() < (Gdx.graphics.getWidth() - 320))){
+            sprite.translateX(speed * delta);
+            System.out.println(Gdx.graphics.getWidth());
+            System.out.println(sprite.getX());
+        }
+        if ((Gdx.input.isKeyPressed(Input.Keys.LEFT)) && (sprite.getX() > 0)) {
+            sprite.translateX(-speed * delta);
+        }
+
     }
 }
