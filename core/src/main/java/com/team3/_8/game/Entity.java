e1490dc79aa2b6ffd6c34f8d401b0108ed1f8596
Lenny Schwarz <lennyschwarz06@gmail.com>
2025-10-22 11:50:32 +0100

    Config: Added some JavaDocs to explain the entity class

--- core/src/main/java/com/team3/_8/game/Entity.java
+++ core/src/main/java/com/team3/_8/game/Entity.java
@@ -4,12 +4,22 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Rectangle;
 
+
+/**
+ * This is the class for all the entities in the game, to inherit from.
+ * @author Lenny
+ */
 public class Entity {
 
     private Sprite sprite;
     private float speed;
     private Rectangle collisionBox;
 
+    /**
+     * Creates the entity based on the sprite given and creates its collision box
+     * @param sprite This is the sprite passed through, containing the position and the texture
+     * @param speed The speed of the entity
+     */
     public Entity(Sprite sprite, float speed) {
         this.sprite = sprite;
         this.speed = speed;
@@ -17,7 +27,7 @@ public class Entity {
     }
 
     public void draw (Batch batch) {
-        this.sprite.draw(batch);
+        this.sprite.draw(batch); // Draws the sprite on the spritebatch provided
     }
 
     public Sprite getEntity() {
