34786605d929a32bf41c1c1afe7ac154f7aeead5
Lenny Schwarz <lennyschwarz06@gmail.com>
2025-10-22 11:32:32 +0100

    Feat: Created the Bob class, for Bob

---
+++ core/src/main/java/com/team3/_8/game/Bob.java
@@ -0,0 +1,11 @@
+package com.team3._8.game;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class Bob extends Entity
+{
+    public Bob(Sprite sprite, float speed)
+    {
+        super(sprite, speed);
+    }
+}
