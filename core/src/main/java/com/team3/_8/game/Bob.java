39245594cb38051b6ccef9b8b0f907906e357506
Lenny Schwarz <lennyschwarz06@gmail.com>
2025-10-22 11:51:30 +0100

    Config: Added some JavaDocs to explain the Bob class

--- core/src/main/java/com/team3/_8/game/Bob.java
+++ core/src/main/java/com/team3/_8/game/Bob.java
@@ -2,6 +2,10 @@ package com.team3._8.game;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
+/**
+ * This is the class for the character (Bob), inheriting from Entity
+ * @author Lenny
+ */
 public class Bob extends Entity
 {
     public Bob(Sprite sprite, float speed)
