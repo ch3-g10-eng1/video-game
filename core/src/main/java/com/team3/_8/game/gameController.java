67c582c921753765980c5114e03f98a0b7fdf7c6
Lenny Schwarz <lennyschwarz06@gmail.com>
2025-10-27 12:08:46 +0000

    Config: Made sure the pause function works and moved the handle input into the class

--- core/src/main/java/com/team3/_8/game/gameController.java
+++ core/src/main/java/com/team3/_8/game/gameController.java
@@ -1,6 +1,10 @@
 package com.team3._8.game;
 
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
 /**
  * This class will be for all the static methods that influence the game
  * and are currently taking up space
@@ -25,6 +29,30 @@ public class gameController {
         secs = (int)currentTime % 60;
 
         return "Timer: " + mins + ":" + String.format("%02d", secs);
+    }
+
+    /**
+     * This method handles the inputs that aren't related to the player input
+     * this includes: camera zooming and pausing
+     * @param camera Orthographic camera: The camera that can zoom in and out
+     * @param paused boolean: The paused state of the game (true if paused)
+     * @return the paused state of the game
+     */
+    public static boolean handleInput(OrthographicCamera camera, boolean paused) {
+        //When Q is pressed, the camera is zoomed in, and zoomed out when E is pressed
+        if (Gdx.input.isKeyPressed(Input.Keys.Q)) {
+            camera.zoom += 0.02f;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.E)) {
+            camera.zoom -= 0.02f;
+        }
+
+        //This is to pause the game
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+            paused = !paused;
+        }
+
+        return paused;
     }
 
 }
