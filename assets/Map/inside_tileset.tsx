86a2afe7785d94fde30224cc1501e1fba8a9e8ff
IsaacMile <isaacjmileham@gmail.com>
2025-11-01 16:11:22 +0000

    feat: First draft of CSE maze map + tilesets for the map. no collision or renderable map yet

---
+++ assets/Map/inside_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="inside_tileset" tilewidth="20" tileheight="20" tilecount="160" columns="16">
+ <image source="Inside_tileset_img.bmp" width="320" height="200"/>
+</tileset>
