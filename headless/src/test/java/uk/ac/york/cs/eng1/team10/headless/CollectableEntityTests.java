2cdbcf03774296af80c8fa13b58f5445a52873d7
harrisbyrom <zhv506@york.ac.uk>
2025-12-11 01:01:13 +0000

    Create CollectableEntityTests.java
    Added testing for CollectableEntity class

---
+++ headless/src/test/java/uk/ac/york/cs/eng1/team10/headless/CollectableEntityTests.java
@@ -0,0 +1,58 @@
+package uk.ac.york.cs.eng1.team10.headless;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.team3._8.game.Bob;
+import com.team3._8.game.CollectableEntity;
+
+public class CollectableEntityTests extends AbstractHeadlessGdxTest {
+  private Sprite stubSprite;
+  private CollectableEntity collectableEntity;
+  @BeforeEach
+  public void createCollectableEntity() {
+    Texture texture = new Texture(new Pixmap(1, 1, Pixmap.Format.RGBA8888));
+    stubSprite = new Sprite(texture);
+    stubSprite.setPosition(100, 100);
+    collectableEntity = new CollectableEntity(stubSprite, 0, "TestType");
+  }
+
+  @Test
+  public void testCollectableEntityCreation() {
+    assertEquals(0, collectableEntity.getSpeed());
+    assertEquals(1, collectableEntity.getCollisionBox().getWidth());
+    assertEquals(1, collectableEntity.getCollisionBox().getHeight());
+    assertEquals(100, collectableEntity.getCollisionBox().getX());
+    assertEquals(100, collectableEntity.getCollisionBox().getY());
+  }
+
+  @Test
+  public void testCollectableEntityCollected() {
+    // Create a Bob instance to collect the entity
+    TextureAtlas atlas = new TextureAtlas("atlas/bob.atlas");
+    Sprite bobSprite = new Sprite(atlas.findRegion("front-bob"));
+    bobSprite.setPosition(100, 100);
+    bobSprite.setSize(15, 15);
+    Bob bob = new Bob(bobSprite, 60, 0);
+    assertEquals(true, collectableEntity.collected(bob));
+    // Collecting again should return false as it is already collected
+    assertEquals(false, collectableEntity.collected(bob));
+  }
+
+  @Test
+  public void testCollectableEntityNotCollected() {
+    // Create a Bob instance to collect the entity
+    TextureAtlas atlas = new TextureAtlas("atlas/bob.atlas");
+    Sprite bobSprite = new Sprite(atlas.findRegion("front-bob"));
+    bobSprite.setPosition(500, 500);
+    bobSprite.setSize(15, 15);
+    Bob bob = new Bob(bobSprite, 60, -3);
+    // Should not be able to collect as Bob is far away
+    assertEquals(false, collectableEntity.collected(bob));
+  }
+}
